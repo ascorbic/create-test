207ac1b86f8f016151c19e306c6b49de2388fbe6
Matt Kane <matt@gatsbyjs.com>
2020-10-27 15:36:13 +0000

    Get install working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { prompt } from "enquirer";
+import path from "path";
+import fs from "fs-extra";
 import questions from "./questions.json";
+import { initStarter } from "./init-starter";
+
+interface IAnswers {
+    project: string;
+}
+
 export async function run() {
     console.log(`Welcome! Let's ask some questions`);
-    const data = await prompt(questions);
+    const data = await prompt<IAnswers>(questions);
+
     console.log(data);
+
+    await initStarter(
+        "https://github.com/gatsbyjs/gatsby-starter-hello-world.git",
+        data.project
+    );
 }
