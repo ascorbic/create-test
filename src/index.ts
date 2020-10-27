ded8a8568f5705abf4237d413084177156a51d1e
ascorbic <ascorbic@gmail.com>
2020-10-27 08:53:54 +0000

    Use seperate cli file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { prompt } from "enquirer";
 import questions from "./questions.json";
-async function run() {
+export async function run() {
     console.log(`Welcome! Let's ask some questions`);
     const data = await prompt(questions);
     console.log(data);
 }
-
-run().then(() => console.log("done"));
