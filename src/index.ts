e97398a03013f2b2933322131b92c5dff3c17390
Matt Kane <matt@gatsbyjs.com>
2020-12-16 10:57:23 +0000

    Just a test

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import { prompt } from "enquirer";
-import path from "path";
-import fs from "fs-extra";
-import questions from "./questions.json";
-import { initStarter } from "./init-starter";
-
-interface IAnswers {
-    project: string;
-}
-
 export async function run() {
-    console.log(`Welcome to Gatsby! Let's answer some questions`);
-    const data = await prompt<IAnswers>(questions);
-
-    await initStarter(
-        "https://github.com/gatsbyjs/gatsby-starter-hello-world.git",
-        data.project
-    );
-    console.log(
-        "Looks like you should probably go and implement `gatsby plugin add` then!"
-    );
+    console.log(`Welcome to test ${require("../package.json").version}`);
 }
